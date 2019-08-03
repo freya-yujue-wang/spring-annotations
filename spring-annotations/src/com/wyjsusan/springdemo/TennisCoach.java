d550d16c4bcfecefa97a63dd545701208bb0c2f8
wyjsusan <yujuewa@gmail.com>
2019-08-02 17:17:08 -0700

    java annotations

--- spring-annotations/src/com/wyjsusan/springdemo/TennisCoach.java
+++ spring-annotations/src/com/wyjsusan/springdemo/TennisCoach.java
@@ -1,17 +1,36 @@
 package com.wyjsusan.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 	
+	@Autowired
+	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
 	
+	//define a default constructor
+	public TennisCoach () {
+		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	/*
+	//define a setter method
+	@Autowired
+	public void doSomeCrazyStuff(FortuneService fortuneService) {
+		System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
+		this.fortuneService = fortuneService;
+	}
+	*/
+	
+	/*
 	@Autowired
 	public TennisCoach(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
+	*/
 
 	@Override
 	public String getDailyWorkout() {
